d4d749d97a783f8cabccf58f9de57cbce2883f09
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-03-31 00:41:23 +0600

    o

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 	"github.com/gofiber/template/html"
 	"github.cpm/ekbal41/fiber-test-1/route"
 )
-func setupRoutes(app *fiber.App) {
 
+// -------------------setupRoutes sets up all the routes for the application------------------>
+func setupRoutes(app *fiber.App) {
 	route.HomeRoutes(app.Group("/"))
 	apiHome := app.Group("/api")
 	route.TodoRoutes(apiHome.Group("/todo"))
@@ -19,15 +20,35 @@ func setupRoutes(app *fiber.App) {
 		return c.SendString("You are in the api endpoint!")
 
 	})
+}
 
-	
+
+
+
+
+
+// ----------------------------------- returns custom 404 page--------------------------------->
+func notFound(c *fiber.Ctx) error {
+    // return c.Status(fiber.StatusNotFound).SendString("Sorry can't find that!")
+	// or render view
+	return c.Status(fiber.StatusNotFound).Render("views/404", fiber.Map{})
 
 }
+
+
+
+
+
+//---------------------------------------all static file setup---------------------------->
 //go:embed views/*
 var viewsfs embed.FS
 //go:embed static/*
 var staticfs embed.FS
 
+
+
+
+//-------------------------------main function----------------------------------------->
 func main(){
 	// viewEngine := html.New("./views", ".html")
 	viewEngine := html.NewFileSystem(http.FS(viewsfs), ".html")
@@ -39,11 +60,14 @@ func main(){
 
 		},
 	)
+	// app.Static("/static", "./static")
     app.Use("/static", filesystem.New(filesystem.Config{
 		Root: http.FS(staticfs),
 		PathPrefix: "static",
 		Browse: true,
 }))
 	setupRoutes(app)
+	//this should be after all the routes
+	app.Use(notFound)
 	log.Fatal(app.Listen(":3000"))
 }
